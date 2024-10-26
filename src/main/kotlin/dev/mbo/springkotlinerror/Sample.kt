fb1c055b8b44abf1e7e06c02e89eb7811d9fab4c
Manuel Bogner <manuel.bogner@openresearch.com>
2024-10-26 05:29:21 +0200

    Initial import

---
+++ src/main/kotlin/dev/mbo/springkotlinerror/Sample.kt
@@ -0,0 +1,5 @@
+package dev.mbo.springkotlinerror
+
+enum class Sample {
+    TESTING
+}
---
+++ src/test/kotlin/dev/mbo/springkotlinerror/SampleTest.kt
@@ -0,0 +1,12 @@
+package dev.mbo.springkotlinerror
+
+import org.junit.jupiter.api.Test
+
+class SampleTest {
+
+    @Test
+    internal fun contextLoads() {
+        // do nothing
+    }
+
+}
